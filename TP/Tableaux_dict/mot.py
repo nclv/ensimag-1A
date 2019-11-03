733e215fc3faa6c0dbc233a892f870451169a49d
Nicolas Vincent <nicolas.vincent100@gmail.com>
2019-11-03 20:12:55 +0100

    Correction de mots.py (choices() retourne une liste)

--- TP/Tableaux_dict/mot.py
+++ TP/Tableaux_dict/mot.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/env python3
+
 """
 On fait une analyse de texte pour dessiner le graphe des mots suivants.
 Permet l'utilisation de dictionnaires et une imbrication de structures.
 On se sert des donnees pour generer des phrases aleatoires.
 """
+
+
 import sys
 from re import finditer
-from random import choice
+from random import choice, choices
 from os import system
 
 
@@ -38,8 +42,17 @@ def compte_mots_suivants(nom_fichier):
     un dict associant a chaque mot m2 suivant m1 dans le fichier
     le nombre de fois ou m2 apparait apres m1.
     """
-    # TODO
-    pass
+    suivants = dict()
+
+    for mot_precedent, mot in couples(mots(nom_fichier)):
+        key_already_exist = mot_precedent in suivants.keys() and mot in suivants[mot_precedent].keys()
+        if key_already_exist:
+            suivants[mot_precedent][mot] += 1
+        else:
+            suivants.setdefault(mot_precedent, {})[mot] = 1
+    print(suivants)
+    return suivants
+
 
 
 def affiche_graphe(suivants):
@@ -48,11 +61,14 @@ def affiche_graphe(suivants):
     attention : petits textes seulement.
     """
     with open("test.dot", "w") as fichier_dot:
-        # TODO : ecrire dans le fichier
-        pass
+        fichier_dot.write('digraph g {\n')
+        for mot1, dictionnaire in suivants.items():
+            for mot2, value in dictionnaire.items():
+                  fichier_dot.write(f'{mot1} -> {mot2} [ label={value} ];\n')
+        fichier_dot.write('}\n')
 
     system("dot -Tpng test.dot -o test.png")
-    system("tycat test.png")
+    system("display test.png")
 
 
 def analyse_texte():
@@ -64,11 +80,12 @@ def analyse_texte():
         print("utilisation :", sys.argv[0], "fichier_texte")
         sys.exit(1)
     suivants = compte_mots_suivants(sys.argv[1])
-    affiche_graphe(suivants)
+    #affiche_graphe(suivants)
     # une petite phrase aleatoire.
     mot_depart = choice(list(suivants.keys()))
     phrase = [mot_depart]
     for _ in range(10):
+        #print("phrase", phrase)
         phrase.append(suivant_aleatoire(phrase[-1], suivants))
     print(" ".join(phrase))
 
@@ -79,8 +96,16 @@ def suivant_aleatoire(mot, suivants):
     un suivant du mot donne.
     si le mot donne n'a pas de suivant, retourne un mot aleatoire.
     """
-    # TODO:
-    pass
+    result = None
+    #print(mot)
+    weights, mots = [], []
+    somme = sum(list(suivants[mot].values()))
+    for mot, value in suivants[mot].items():
+        weights.append(value/somme)
+        mots.append(mot)
+    #print(somme, mots, weights)
+    result = mots[0] if len(mots) == 1 else choices(mots, weights=weights)[0]
+    return result
 
 if __name__ == "__main__":
     analyse_texte()
