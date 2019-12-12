2dc13000993bf3269179ec1c48e8153a3999e1b4
Nicolas Vincent <nicolas.vincent100@gmail.com>
2019-12-12 12:13:46 +0100

    remove pyhack

--- Projets/pyhack/pyhack/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "1.0.0"
-__author__ = "VINCENT Nicolas" + "Alan Dione"
-__licence__ = "GPLv3"
--- Projets/pyhack/pyhack/launch.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""launch.py: Lance le jeu."""
-
-from pyhack import pyhack
-
-pyhack.main()
